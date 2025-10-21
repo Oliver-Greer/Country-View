a0b0b747e2544da4f11a554590fc0f080b917df4
Oliver Greer <jollygreer2000@gmail.com>
2025-10-21 12:32:59 -0700

    Third try at building

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json == {"app": "online"}
+    assert response.json() == {"app": "online"}
