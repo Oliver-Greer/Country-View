e817db4e7fd90fe9e84b80787df01037a33ce736
Oliver Greer <jollygreer2000@gmail.com>
2025-10-26 17:04:12 -0700

    Moved name state up a level

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,67 @@
 import "./App.css";
 import SideBar from "./SideBar/SideBar.tsx";
 import USMap from "./USMap/USMap.tsx";
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 
-function App() {
-  const [isStateClicked, setStateClicked] = useState(
+export type SelectedStateState = {
+  selectedName: string;
+  isStateClicked: boolean;
+};
+
+type SetStateSelected = {
+  type: "SET_STATE_SELECTED";
+  payload: string;
+};
+
+type SetStateNotSelected = {
+  type: "SET_STATE_NOT_SELECTED";
+};
+
+export type ClickStateAction = SetStateNotSelected | SetStateSelected;
+
+const selectedStateReducer = (
+  state: SelectedStateState,
+  action: ClickStateAction,
+) => {
+  switch (action.type) {
+    case "SET_STATE_SELECTED":
+      return { ...state, isStateClicked: true, selectedName: action.payload };
+    case "SET_STATE_NOT_SELECTED":
+      return { ...state, isStateClicked: false, selectedName: "" };
+    default:
+      throw new Error();
+  }
+};
+
+const initialStateState: SelectedStateState = {
+  isStateClicked:
     JSON.parse(localStorage.getItem("isStateClicked") || "false") || false,
-  ); // JSON.parse cannot take null values so check for key: "false" instead
+  selectedName: localStorage.getItem("selectedName") || "",
+};
+
+// move state up to App level
+function App() {
+  const [selectedStateState, dispatchSelectedState] = useReducer(
+    selectedStateReducer,
+    initialStateState,
+  );
+
   useEffect(() => {
-    localStorage.setItem("isStateClicked", isStateClicked.toString());
-  }, [isStateClicked]);
+    localStorage.setItem("selectedName", selectedStateState.selectedName);
+  }, [selectedStateState.selectedName]);
+  useEffect(() => {
+    localStorage.setItem(
+      "isStateClicked",
+      selectedStateState.isStateClicked.toString(),
+    );
+  }, [selectedStateState.isStateClicked]);
 
   return (
     <>
       <div className="pageLayout z-0 w-screen bg-linear-to-t from-zinc-800 to-red-950">
         <div>
-          <SideBar isStateClicked={isStateClicked} />
-          <USMap setStateClicked={setStateClicked} />
+          <SideBar selectedStateState={selectedStateState} />
+          <USMap dispatchSelectedState={dispatchSelectedState} />
         </div>
       </div>
     </>
--- frontend/src/SideBar/SideBar.tsx
+++ frontend/src/SideBar/SideBar.tsx
@@ -1,10 +1,12 @@
+import type { SelectedStateState } from "../App.tsx";
+
 interface SideBarProps {
-  isStateClicked: boolean;
+  selectedStateState: SelectedStateState;
 }
 
-const SideBar: React.FC<SideBarProps> = ({ isStateClicked }) => {
+const SideBar: React.FC<SideBarProps> = ({ selectedStateState }) => {
   let transitionState = "transform translate-x-0";
-  if (!isStateClicked) {
+  if (!selectedStateState.isStateClicked) {
     transitionState = "transform -translate-x-[25vw]";
   }
   const baseClasses =
@@ -12,21 +14,26 @@ const SideBar: React.FC<SideBarProps> = ({ isStateClicked }) => {
 
   //get data from backend and map over it
   return (
-    <div className={`${baseClasses} ${transitionState}`}>
-      <div className="w-full flex flex-col items-center pt-10 px-4 overflow-y-auto max-h-full">
-        <p className="text-center text-2xl md:text-4xl lg:text-6xl font-sans font-bold mb-20">
-          Representatives
-        </p>
-        <div className="w-full space-y-4 flex-grow">
-          <p className="text-center text-1xl md:text-2xl lg:text-4xl font-sans font-bold">
-            1
+    <div>
+      <p className="absolute z-3 top-10 inset-x-0 text-2xl md:text-4xl lg:text-6xl text-center text-zinc-300 font-sans font-bold">
+        {selectedStateState.selectedName}
+      </p>
+      <div className={`${baseClasses} ${transitionState}`}>
+        <div className="w-full flex flex-col items-center pt-10 px-4 overflow-y-auto max-h-full">
+          <p className="text-center text-2xl md:text-4xl lg:text-6xl font-sans font-bold mb-20">
+            Representatives
           </p>
-          <p className="text-center text-1xl md:text-2xl lg:text-4xl font-sans font-bold">
-            2
-          </p>
-          <p className="text-center text-1xl md:text-2xl lg:text-4xl font-sans font-bold">
-            3
-          </p>
+          <div className="w-full space-y-4 flex-grow">
+            <p className="text-center text-1xl md:text-2xl lg:text-4xl font-sans font-bold">
+              1
+            </p>
+            <p className="text-center text-1xl md:text-2xl lg:text-4xl font-sans font-bold">
+              2
+            </p>
+            <p className="text-center text-1xl md:text-2xl lg:text-4xl font-sans font-bold">
+              3
+            </p>
+          </div>
         </div>
       </div>
     </div>
